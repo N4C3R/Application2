5421f6ac92f5b2da9c1e37af5f0bf1c60e2915c6
N4C3R <k-974@outlook.fr>
2017-10-19 14:14:12 +0100

    Ajout de la classe demo pour tester les methodes

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+	VectorHelper v1=new VectorHelper();
+	
+	v1.addVectElemnt(4);
+	v1.addVectElemnt(1);
+	v1.addVectElemnt(3);
+	v1.addVectElemnt(2);
+	v1.Trie();
+	v1.Afficher();
+	
+	return;
+	
+	}
+
+}
